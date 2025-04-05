d515dfc13fe9dc2ce38b5197a279af95a1fdf7d2
Ivan Sevryukov <sevryukovivan1995@mail.ru>
2025-04-05 18:05:44 +0300

    First and second solutions

--- 01-exercise/01-goroutines/02-client-server/server/main.go
+++ 01-exercise/01-goroutines/02-client-server/server/main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"io"
+	"log"
 	"net"
 	"time"
 )
 
 func main() {
 	// TODO: write server program to handle concurrent client connections.
+
+	// Listen for incoming connections.
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	for {
+		// Accept the connection.
+		con, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		// Read and optionally write data to the connection.
+		go handleConn(con)
+	}
 
 }
 
